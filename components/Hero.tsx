82608434c66326eab94d6bec7b0adbe2e53fa7a8
Bryan Millon <bryanmillon31@gmail.com>
2023-01-09 19:54:25 -0600

    Hero section finish

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,56 @@
 import React from 'react'
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
+import BackgroundCircles from './BackgroundCircles';
+import Image from 'next/image'
+import profilePic from  './img/ME.jpg'
+import Link from 'next/link';
 
 type Props = {}
 
 export default function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: ['Hi, my name is Bryan', 
+    'Full stack developer and friendly neighborhood.',
+    'Coding by day, saving the world with Spiderman by night',
+    'Guy who code for hours with a good cup of coffe by his side.'],
+    loop: true,
+    delaySpeed: 1000
+  });
+  
   return (
-    <div>Hero</div>
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+      <BackgroundCircles/>
+      <Image
+        src={profilePic}
+        alt="Picture of the author"
+        className='relative rounded-full h-36 w-36 mx-auto object-cover'
+      />
+      <div className='z-20'>
+        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>
+          Full Stack Developer
+        </h2>
+      <h1 className='text-5xl lg:text-6x1 font-semibold px-10'>
+      <span className='mr-2'>{text}</span>
+      <Cursor cursorColor="#F7AB0A"/>
+      </h1>
+      <div className='pt-5'>
+        <Link href='#about'>
+        <button className='heroButtons'>About</button>
+        </Link>
+
+        <Link  href='#experience'>
+        <button className='heroButtons'>Experience</button>
+        </Link>
+
+        <Link  href='#skills'>
+        <button className='heroButtons'>Skills</button>
+        </Link>
+
+        <Link  href='#projects'>
+        <button className='heroButtons'>Projects</button>
+        </Link>
+      </div>
+      </div>
+    </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,29 @@
 import Head from 'next/head'
+import About from '../components/About'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 
 
 export default function Home() {
   return (
-    <>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Bryan Millon Portfolio</title>
       </Head>
 
+      {/* HEADER */}
       <Header/>
       
       {/* HERO */}
+  <section id='Hero' className='snap-center'>
+    <Hero/>
+  </section>
 
       {/* ABOUT */}
+      <section id='about'>
+        <About/>
+      </section>
 
       {/* EXPERIENCE  */}
 
@@ -23,6 +32,6 @@ export default function Home() {
       {/* CONTACT ME  */}
 
 
-    </>
+    </div>
   )
 }
