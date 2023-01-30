bc803f344ccd1957780c86aab72a7cb128ffb2da
Bryan Millon <bryanmillon31@gmail.com>
2023-01-30 01:28:56 -0600

    fix skill section

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -10,8 +10,9 @@ type Props = {
 
 export default function Skill({skill}: Props) {
   return (
-    <div className="group relative flex cursor-pointer">
+    <div className="group relative flex cursor-pointer ">
       
+      <div className="flex flex-col space-y-2 ">
       <motion.img
         initial={{
           opacity: 0,
@@ -19,11 +20,16 @@ export default function Skill({skill}: Props) {
         transition={{ duration: 2.5 }}
         whileInView={{ opacity: 1 }}
         src={urlFor(skill?.image).url()}
-        className="rounded-full border  border-gray-500 object-cover w-16 h-16  mobile-m:w-20 mobile-m:h-20  sm:w-28 sm:h-28 height-800:w-24 height-800:h-24  height-1000:w-36 height-1000:h-36 filter group-hover:grayscale transition duration-300 ease-in-out"
+        className="rounded-lg  object-cover w-10 h-10  mobile-m:w-14 mobile-m:h-14 mobile-l:w-16 mobile-l:h-16 sm:w-24 sm:h-24 height-800:w-20 height-800:h-20  height-1000:w-32 height-1000:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
       />
-      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-16 h-16   sm:w-28 sm:h-28 mobile-m:w-20 mobile-m:h-20 height-800:w-24 height-800:h-24  height-1000:w-36 height-1000:h-36 rounded-full z-0">
-        <div className="flex items-center justify-center h-full">
-          <p className="text-2xl mobile-m:text-3xl font-bold text-black opacity-100">
+      <h4 className="font-semibold text-center text-xs mobile-l:text-sm height-800:text-base height-1000:text-2xl">{skill?.title}</h4>
+          </div>
+      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-10 h-10   sm:w-24 sm:h-24 mobile-m:w-14 mobile-m:h-14 mobile-l:w-16 mobile-l:h-16  height-800:w-20 height-800:h-20 height-1000:w-32 height-1000:h-32 rounded-lg z-0">
+        <div className="flex items-center justify-evenly h-full">
+          {/* <p className="text-sm mobile-m:text-3xl font-bold text-black opacity-100">
+            {skill?.title}
+          </p> */}
+          <p className="text-lg mobile-m:text-2xl mobile-l:text-3xl height-1000:text-5xl font-bold text-black opacity-100">
            {skill?.progress}%
           </p>
         </div>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -13,12 +13,12 @@ export default function Skills({skills}: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="flex relative flex-col justify-center items-center text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen xl:space-y-0 mx-auto "
+      className="flex relative flex-col justify-center items-center text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen xl:space-y-0 mx-auto overflow-scroll "
     >
       <h3 className="absolute top-16 height-800:top-20 uppercase tracking-[20px] text-gray-500 text-xl mobile-xl:text-2xl ">
         Skills
       </h3>
-      <div className="grid grid-cols-3 sm:grid-cols-4 gap-3 mobile-m:gap-5">
+      <div className="grid grid-cols-4 sm:grid-cols-5 gap-3  height-1000:gap-7" >
         {skills?.map(skill => (
           <Skill key={skill._id} skill={skill}/>
         ))}
